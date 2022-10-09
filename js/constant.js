321d5530f3e2f965fcba4225b7e0af75c53d0a14
Mart Anthony Salazar <martsalazar0@gmail.com>
2022-10-09 12:58:47 +0800

    add constants

---
+++ js/constant.js
@@ -0,0 +1,38 @@
+const projects = [
+  {
+    name: "Reactive OS",
+    link: "https://react-os.netlify.app/",
+    sourcecode: "https://github.com/mart-anthony-stark/Reactive-OS",
+    description: "Reactive OS is a desktop environment UI built in ReactJS",
+    lang: "Javascript",
+  },
+  {
+    name: "Pizza Web App",
+    sourcecode: "https://github.com/mart-anthony-stark/pizza-orders-api",
+    description: "",
+    lang: "Javascript",
+  },
+  {
+    name: "Expice",
+    link: "https://expice-resto.netlify.app",
+    sourcecode:
+      "https://github.com/mart-anthony-stark/Expice-Restaurant-Vue-FE",
+    description: "",
+    lang: "Vue",
+  },
+  {
+    name: "Reactive OS",
+    link: "https://react-os.netlify.app/",
+    sourcecode: "https://github.com/mart-anthony-stark/Reactive-OS",
+    description: "",
+    lang: "Javascript",
+  },
+];
+
+const langColors = {
+  Javascript: "#F0E05B",
+  Vue: "#41B982",
+  Python: "#3573A4",
+  Typescript: "#2b7489",
+  Jupyter: "#DA5B0B",
+};
